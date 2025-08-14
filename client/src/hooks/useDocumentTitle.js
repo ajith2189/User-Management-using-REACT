7b89e76695f97c409032fdd4f5e50a5aa4c34984
Ajith <130132590+ajith2189@users.noreply.github.com>
2025-08-14 10:00:15 +0530

    Update useDocumentTitle.js

--- client/src/hooks/useDocumentTitle.js
+++ client/src/hooks/useDocumentTitle.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-
+// this is the hook used to make the page as the title
 const useDocumentTitle = (name) => {
     useEffect(() => {
         document.title = name

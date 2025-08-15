b71f1b38a4b02bebc303306b04d4f9e34ea80004
Ajith <130132590+ajith2189@users.noreply.github.com>
2025-08-15 22:20:52 +0530

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,7 +2,7 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const { generateAccessToken } = require("../utils/generateToken");
 const hashPassword = require("../utils/hashPassword"); 
-//some are the most
+//some are the most important
 
 const loginAdmin = async (req, res) => {
   console.log("admin login called");

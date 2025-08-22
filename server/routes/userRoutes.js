c6ef8d1c330b37fb924c9183618a9eee36ce4357
Ajith <130132590+ajith2189@users.noreply.github.com>
2025-08-22 09:16:56 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const user_router = express.Router();
-// this is the most important and the common 
+// this is the most important and the  
 const{registerUser,
     loginUser,
     uploadImage,
